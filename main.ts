5778d1db34ca32f450d682c044fea787f2d9242b
bryan weinstein <bryanweinstein@outlook.com>
2022-01-13 12:15:16 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+let onoff = 0
 let lights = 0
+input.onButtonPressed(Button.A, function () {
+    if (onoff == 0) {
+        onoff = 1
+    } else {
+        onoff = 0
+    }
+})
 function squidLight () {
     for (let index = 0; index < 4; index++) {
         if (lights == 0) {
@@ -6,34 +14,37 @@ function squidLight () {
         } else {
             lights = 0
         }
-        pins.digitalWritePin(DigitalPin.P1, lights)
-        pins.digitalWritePin(DigitalPin.P2, lights)
-        pins.digitalWritePin(DigitalPin.P3, lights)
+        pins.digitalWritePin(DigitalPin.P12, lights)
+        pins.digitalWritePin(DigitalPin.P13, lights)
+        pins.digitalWritePin(DigitalPin.P14, lights)
         basic.pause(1000)
     }
 }
 function blobfishOne (onoff: number) {
     if (onoff == 1) {
-        servos.P0.run(-31)
+        servos.P2.run(-31)
     } else {
-        servos.P0.run(0)
+        servos.P2.run(0)
     }
 }
 function blobfishTwo (onoff: number) {
     if (onoff == 1) {
-        servos.P0.run(63)
+        servos.P1.run(63)
     } else {
-        servos.P0.run(0)
+        servos.P1.run(0)
     }
 }
 basic.forever(function () {
-    if (input.lightLevel() > 100) {
-        blobfishOne(1)
-        blobfishTwo(1)
-    } else {
-        blobfishOne(0)
-        blobfishTwo(0)
-        squidLight()
+    if (onoff == 1) {
+        servos.P0.run(50)
+        if (input.lightLevel() > 100) {
+            blobfishOne(1)
+            blobfishTwo(1)
+        } else {
+            blobfishOne(0)
+            blobfishTwo(0)
+            squidLight()
+        }
     }
     basic.pause(5000)
 })
